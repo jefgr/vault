e4fba212c3fdff74045164fb1974b68bca32a1f5
jef <jef.grupping@gmail.com>
2025-08-26 16:23:23 +0200

    vault backup: 2025-08-26 16:23:23

--- batman.md
+++ batman.md
@@ -1,3 +1,1 @@
 Batman
-
-a + b = a + 1 (b keer
---
+++ schakel/wetenschappelijk_rekenen/wpo/python_project/visualise.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+
+def format_data_for_visualisation(start, dur):
+    """Formats the start variables a list of dictionaries in a certain format required by the visualisation code."""
+    job_dic = {
+        0: "Paper_1",
+        1: "Paper_2",
+        2: "Paper_3"
+    }
+
+    machine_dic = {
+        0: "Green",
+        1: "Blue",
+        2: "Yellow"
+    }
+    results = []
+    for key in start.keys():
+        job, machine = key
+        result = {
+            "Job": job_dic[job],
+            "Machine": machine_dic[machine],
+            "Start": start[key].varValue,
+            "Duration": dur[job][machine],
+            "Finish": start[key].varValue + dur[job][machine]
+        }
+        results.append(result)
+    return results
+
+def visualize(results):
+    """Produces a Gantt chart of the job shop schedule."""
+
+    schedule = pd.DataFrame(results)
+    JOBS = sorted(list(schedule['Job'].unique()))
+    MACHINES = sorted(list(schedule['Machine'].unique()))
+    makespan = schedule['Finish'].max()
+    
+    bar_style = {'alpha':1.0, 'lw':25, 'solid_capstyle':'butt'}
+    text_style = {'color':'white', 'weight':'bold', 'ha':'center', 'va':'center'}
+    colors = mpl.cm.Dark2.colors
+
+    schedule.sort_values(by=['Job', 'Start'])
+    schedule.set_index(['Job', 'Machine'], inplace=True)
+
+    fig, ax = plt.subplots(2,1, figsize=(12, 5+(len(JOBS)+len(MACHINES))/4))
+
+    for jdx, j in enumerate(JOBS, 1):
+        for mdx, m in enumerate(MACHINES, 1):
+            if (j,m) in schedule.index:
+                xs = schedule.loc[(j,m), 'Start']
+                xf = schedule.loc[(j,m), 'Finish']
+                ax[0].plot([xs, xf], [jdx]*2, c=colors[mdx%7], **bar_style)
+                ax[0].text((xs + xf)/2, jdx, m, **text_style)
+                ax[1].plot([xs, xf], [mdx]*2, c=colors[jdx%7], **bar_style)
+                ax[1].text((xs + xf)/2, mdx, j, **text_style)
+                
+    ax[0].set_title('Job Schedule')
+    ax[0].set_ylabel('Job')
+    ax[1].set_title('Machine Schedule')
+    ax[1].set_ylabel('Machine')
+    
+    for idx, s in enumerate([JOBS, MACHINES]):
+        ax[idx].set_ylim(0.5, len(s) + 0.5)
+        ax[idx].set_yticks(range(1, 1 + len(s)))
+        ax[idx].set_yticklabels(s)
+        ax[idx].text(makespan, ax[idx].get_ylim()[0]-0.2, "{0:0.1f}".format(makespan), ha='center', va='top')
+        ax[idx].plot([makespan]*2, ax[idx].get_ylim(), 'r--')
+        ax[idx].set_xlabel('Time')
+        ax[idx].grid(True)
+        
+    fig.tight_layout()
